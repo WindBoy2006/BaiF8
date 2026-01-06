c5dd55d9fd443d54fd89bea6441ea1499f0b6d9f
WindBoy2006 <phongtuankhang@gmail.com>
2026-01-06 20:55:08 +0700

    add new content

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-console.log("hello anh em")
-console.log("ok anh em")
-console.log(1000, 2000, "OK ANH EM")
+// console.log("hello anh em")
+// console.log("ok anh em")
+// console.log(1000, 2000, "OK ANH EM")
+
+//khoi tao bien (init)
+const a = 1
+let b = 2
+var c = 3
