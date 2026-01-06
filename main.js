83e5bd38be1a4e4b0024872272af2cc2a4a09db7
WindBoy2006 <phongtuankhang@gmail.com>
2026-01-06 21:50:22 +0700

    add new content

--- main.js
+++ main.js
@@ -2,7 +2,28 @@
 // console.log("ok anh em")
 // console.log(1000, 2000, "OK ANH EM")
 
+//khai bao bien
+//let x, y, z
+//x = "ok anh em"
+//y = 200
+//z = 300
+
 //khoi tao bien (init)
-const a = 1
-let b = 2
-var c = 3
+//const a = 1
+//let b = 2
+//var c = 3
+
+//console.log(a, b, c, x, y, z)
+
+// let a
+
+// a = 2
+
+let a = 100
+console.log(typeof a)
+
+const isNumber = true
+console.log(typeof isNumber)
+
+const name = "phongnt"
+console.log(typeof name)
